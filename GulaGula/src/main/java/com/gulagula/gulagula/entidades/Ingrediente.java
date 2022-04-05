cc146b1cfe554335535a7590b4c31b5477e26f8a
Nicolas Ledesma <nicolasexequiel2010@gmail.com>
2022-04-05 19:50:14 -0300

    Completo atributos de entidades y enum

--- GulaGula/src/main/java/com/gulagula/gulagula/entidades/Ingrediente.java
+++ GulaGula/src/main/java/com/gulagula/gulagula/entidades/Ingrediente.java
@@ -1,7 +1,16 @@
 package com.gulagula.gulagula.entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
 public class Ingrediente {
-    
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     private String nombre;
 }
---
+++ GulaGula/src/main/java/com/gulagula/gulagula/enumeradores/Sabor.java
@@ -0,0 +1,5 @@
+package com.gulagula.gulagula.enumeradores;
+
+public enum Sabor {
+    dulce, salado, picante, agridulce
+}
